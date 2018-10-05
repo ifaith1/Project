5e54bcc37a441a734b4efee8c8a068c623dd1e66
Val <applejelly@yahoo.com>
2018-10-05 12:31:55 -0500

    Create jump, create nonability to double jump, create slippery and slow materials, create circle collider 2D

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -11,22 +11,54 @@ public class PlayerCharacter : MonoBehaviour
     private float maxSpeed = 5;
 
     [SerializeField]
+    private float jumpForce = 10;
+
+    [SerializeField]
     private Rigidbody2D rb2d;
 
+    [SerializeField]
+    private Collider2D groundDetectTrigger;
+
+    [SerializeField]
+    private ContactFilter2D groundContactFilter;
+
     private float horizontalInput;
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
+    private bool isOnGround;
+    private Collider2D[] groundHitDetectionResults = new Collider2D[16];
 
     // Update is called once per frame
     void Update()
     {
+        UpdateIsOnGround();
+        UpdateHorizontalInput();
+        HandleJumpInput();
+    }
+
+    private void UpdateIsOnGround()
+    {
+        isOnGround = groundDetectTrigger.OverlapCollider(groundContactFilter, groundHitDetectionResults) > 0;
+        // Debug.Log("IsOnGround?: " + isOnGround);
+    }
+
+    private void UpdateHorizontalInput()
+    {
         horizontalInput = Input.GetAxis("Horizontal");
     }
 
+    private void HandleJumpInput()
+    {
+        if (Input.GetButtonDown("Jump") && isOnGround)
+        {
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
     private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
     {
         rb2d.AddForce(Vector2.right * horizontalInput * accelerationForce);
         Vector2 clampedVelocity = rb2d.velocity;
