43d8f8dd568dcee27cdf9bf7889f8a2b0bcdca8c
Val <applejelly@yahoo.com>
2018-12-04 15:19:38 -0600

    Working on Pickups

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerCharacter : MonoBehaviour
 {
@@ -32,6 +33,36 @@ public class PlayerCharacter : MonoBehaviour
     [SerializeField]
     private ContactFilter2D groundContactFilter;
 
+
+    //public float speed;             //Floating point variable to store the player's movement speed.
+    //public Text countText;          //Store a reference to the UI Text component which will display the number of pickups collected.
+    //public Text winText;            //Store a reference to the UI Text component which will display the 'You win' message.
+
+    //private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    //private int count;              //Integer to store the number of pickups collected so far.
+    //public AudioSource audiosource;
+
+    //// Use this for initialization
+    //void Start()
+    //{
+    //    //Get and store a reference to the Rigidbody2D component so that we can access it.
+    //    rb2d = GetComponent<Rigidbody2D>();
+
+    //    //Initialize count to zero.
+    //    count = 0;
+
+    //    //Initialze winText to a blank string since we haven't won yet at beginning.
+    //    winText.text = "";
+
+    //    //Call our SetCountText function which will update the text with the current value for count.
+    //    SetCountText();
+
+    //    audiosource = GetComponent<AudioSource>();
+    //}
+
+
+
+
     private Animator myAnimator;
     private float horizontalInput;
     private bool isOnGround;
@@ -39,6 +70,9 @@ public class PlayerCharacter : MonoBehaviour
     private Checkpoint currentCheckpoint;
     private bool facingRight = true;
     private AudioSource audioSource;
+    public Text countText;          //Store a reference to the UI Text component which will display the number of pickups collected.
+    public Text winText;            //Store a reference to the UI Text component which will display the 'You win' message.
+    private int count;              //Integer to store the number of pickups collected so far.
 
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -58,6 +92,9 @@ public class PlayerCharacter : MonoBehaviour
         myAnimator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
         audioSource = GetComponent<AudioSource>();
+        count = 0;
+        winText.text = "";
+        SetCountText ();
     }
 
     // Update is called once per frame
@@ -76,6 +113,21 @@ public class PlayerCharacter : MonoBehaviour
         UpdatePhysicsMaterial();
         Move();
         HandleFlipping();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag ("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
     }
 
     private void HandleFlipping()
